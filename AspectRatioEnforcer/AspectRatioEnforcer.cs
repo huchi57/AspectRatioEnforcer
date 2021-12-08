ba9b8409653f539b54bda78676b90eec1d633080
huchi57 <felixhu0312@gmail.com>
2021-12-08 10:19:28 +0800

    Update AspectRatioEnforcer.cs

--- AspectRatioEnforcer/AspectRatioEnforcer.cs
+++ AspectRatioEnforcer/AspectRatioEnforcer.cs
@@ -1,29 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 [DisallowMultipleComponent]
 [RequireComponent(typeof(Camera))]
 public class AspectRatioEnforcer : MonoBehaviour
 {
-    private const float _16to9 = 16f / 9f;
-    private const float _16to10 = 16f / 10f;
-    private const float _21to9 = 21f / 9f;
-    private const float _4to3 = 4f / 3f;
-    private const float _5to4 = 5f / 4f;
-
-    [System.Serializable]
-    public enum AspectRatio
-    {
-        [InspectorName("16:9")] _16to9,
-        [InspectorName("16:10")] _16to10,
-        [InspectorName("21:9")] _21to9,
-        [InspectorName("4:3")] _4to3,
-        [InspectorName("5:4")] _5to4,
-        Custom
-    }
-
-    private class Mask
+    private class Masks
     {
         private Rect _mask1 = new Rect();
         private Rect _mask2 = new Rect();
@@ -33,17 +15,17 @@ public class AspectRatioEnforcer : MonoBehaviour
         public Rect Mask2 => _mask2;
         public Rect Viewport => _viewport;
 
-        public void SetLetterbox(float viewportWidth, float maskWidth, float viewportIncet)
+        public void SetLetterbox(float viewportHeight, float maskHeight, float viewportIncet)
         {
-            _mask1.Set(0, 0, Screen.width, maskWidth);
-            _mask2.Set(0, maskWidth + viewportWidth, Screen.width, maskWidth);
+            _mask1.Set(0, 0, Screen.width, maskHeight);
+            _mask2.Set(0, maskHeight + viewportHeight, Screen.width, maskHeight);
             _viewport.Set(0, viewportIncet / 2, 1, 1 - viewportIncet);
         }
 
-        public void SetPillarbox(float viewportHeight, float maskHeight, float viewportIncet)
+        public void SetPillarbox(float viewportWidth, float maskWidth, float viewportIncet)
         {
-            _mask1.Set(0, 0, maskHeight, Screen.height);
-            _mask2.Set(maskHeight + viewportHeight, 0, maskHeight, Screen.height);
+            _mask1.Set(0, 0, maskWidth, Screen.height);
+            _mask2.Set(maskWidth + viewportWidth, 0, maskWidth, Screen.height);
             _viewport.Set(viewportIncet / 2, 0, 1 - viewportIncet, 1);
         }
 
@@ -55,124 +37,98 @@ public class AspectRatioEnforcer : MonoBehaviour
         }
     }
 
-    [SerializeField] private AspectRatio _aspectRatio = AspectRatio._16to9;
-    [SerializeField] private float _customAspectRatio = 16f / 9f;
+    [SerializeField] private bool _previewInEditMode = true;
     [SerializeField] private Color _maskColor = Color.black;
+    [SerializeField] private float _aspectRatio = 16f / 9f;
 
+    private static Texture2D _maskTexture = null;
+    private static GUIStyle _style = null;
     private Camera _camera = null;
-    private GUIStyle _style = null;
-    private Texture2D _maskTexture = null;
-    private Mask _mask = new Mask();
+    private Masks _mask = null;
 
-    public Color MaskColor
-    {
-        get => _maskColor;
-        set
-        {
-            _maskColor = value;
-            RefreshMaskTexture();
-        }
-    }
+    public bool PreviewInEditMode { get => _previewInEditMode; set => _previewInEditMode = value; }
+    public Color MaskColor { get => _maskColor; set => _maskColor = value; }
+    public float AspectRatio { get => _aspectRatio; set => _aspectRatio = value; }
 
-    private float CameraInset => 1f - (ScreenRatio / TargetRatio);
+    private float ViewportInset => 1f - (ScreenRatio / _aspectRatio);
     private float ScreenRatio => Screen.width / (float)Screen.height;
-
-    private float TargetRatio
+    private Masks Mask
     {
         get
         {
-            switch (_aspectRatio)
+            if (_mask == null)
             {
-                case AspectRatio._16to9: return _16to9;
-                case AspectRatio._16to10: return _16to10;
-                case AspectRatio._21to9: return _21to9;
-                case AspectRatio._4to3: return _4to3;
-                case AspectRatio._5to4: return _5to4;
-                case AspectRatio.Custom: break;
-                default: break;
+                _mask = new Masks();
             }
-            return _customAspectRatio;
+            return _mask;
         }
     }
 
-    private Texture2D MaskTexture
+    private void DrawMask()
     {
-        get
-        {
-            if (_maskTexture == null)
-            {
-                _maskTexture = new Texture2D(1, 1);
-                RefreshMaskTexture();
-            }
-            return _maskTexture;
-        }
-    }
-
-    private GUIStyle Style
-    {
-        get
-        {
-            if (_style == null)
-            {
-                _style = new GUIStyle();
-            }
-            _style.normal.background = MaskTexture;
-            return _style;
-        }
-    }
-
-    public void RefreshMaskTexture()
-    {
-        if (_maskTexture != null)
+        if (_maskTexture == null)
         {
             _maskTexture = new Texture2D(1, 1);
         }
-        _maskTexture.SetPixel(0, 0, MaskColor);
+
+        if (_style == null)
+        {
+            _style = new GUIStyle();
+        }
+
+        _maskTexture.SetPixel(0, 0, _maskColor);
         _maskTexture.Apply();
+        _style.normal.background = _maskTexture;
+
+        GUI.Box(Mask.Mask1, GUIContent.none, _style);
+        GUI.Box(Mask.Mask2, GUIContent.none, _style);
+        _camera.rect = Mask.Viewport;
     }
 
-    private void DrawMasks()
+    private void ResetCamera()
     {
-        GUI.Box(_mask.Mask1, GUIContent.none, Style);
-        GUI.Box(_mask.Mask2, GUIContent.none, Style);
-        _camera.rect = _mask.Viewport;
+        Mask.ClearBox();
+        _camera.rect = Mask.Viewport;
     }
 
     private void OnGUI()
     {
-        if (TargetRatio > ScreenRatio)
+        if (_previewInEditMode || Application.isPlaying)
         {
-            var viewportHeight = Screen.width / TargetRatio;
-            var maskHeight = (Screen.height - viewportHeight) / 2;
-            _mask.SetLetterbox(viewportHeight, maskHeight, CameraInset);
-        }
+            if (_aspectRatio > ScreenRatio)
+            {
+                var viewportHeight = Screen.width / _aspectRatio;
+                var maskHeight = (Screen.height - viewportHeight) / 2;
+                Mask.SetLetterbox(viewportHeight, maskHeight, ViewportInset);
+            }
 
-        else if (TargetRatio < ScreenRatio)
-        {
-            var viewportWidth = Screen.height * TargetRatio;
-            var maskWidth = (Screen.width - viewportWidth) / 2;
-            _mask.SetPillarbox(viewportWidth, maskWidth, CameraInset);
-        }
+            else if (_aspectRatio < ScreenRatio)
+            {
+                var viewportWidth = Screen.height * _aspectRatio;
+                var maskWidth = (Screen.width - viewportWidth) / 2;
+                Mask.SetPillarbox(viewportWidth, maskWidth, ViewportInset);
+            }
 
+            else
+            {
+                Mask.ClearBox();
+            }
+        }
         else
         {
-            _mask.ClearBox();
+            Mask.ClearBox();
         }
 
-        DrawMasks();
+        DrawMask();
     }
 
     private void Awake()
     {
         _camera = GetComponent<Camera>();
-        _mask = new Mask();
     }
 
-    private void OnValidate()
+    private void OnDisable()
     {
-        if (_customAspectRatio < 0.001f)
-        {
-            _customAspectRatio = 0.001f;
-        }
+        ResetCamera();
     }
 }
